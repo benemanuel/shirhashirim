8f030c5df27b230d8bdea87cf1cdf16669f82691
Avi BenEmanuel <benemanuel@geulah.org.il>
2023-04-20 10:47:36 +0300

    first

--- intro/old/signs_body_1.php
+++
@@ -1,5 +0,0 @@
-K: C 
-C8 D8 E8 F8 G8 A8 B8 c8
-w:א֧ א֛ אֽ א֥ א֖ א֑ א֣ א֚
-w: Darga Tevir Sillouk Mercha Tipcha Etnachta Munach Mahpach
-w: דַּרְגָּ֧א תְּבִ֛יר סִלּֽוּק מֵרְכָ֥א טִפְּחָ֖א אֶתְנַחְתָּ֑א מֻנַּ֣ח מַהְפַּ֤ך
